8e33098d15ff8fde5cdf002ca9b0f905572fa0dc
rohn-allen-santiago <rsantiag@tcd.ie>
2025-11-24 15:19:01 +0000

    Add word separation and headline and summary fields

--- src/main/java/org/cs7is3/Searcher.java
+++ src/main/java/org/cs7is3/Searcher.java
@@ -197,15 +197,30 @@ public class Searcher {
         StringBuilder sb = new StringBuilder();
 
         // Boost title higher
-        if (!topic.title.isEmpty()) sb.append("headline:(").append(QueryParserBase.escape(topic.title)).append(")^8");
+        if (!topic.title.isEmpty()) {
+            String[] splitTitle = topic.title.split(" ");
+            for(String word : splitTitle) {
+                sb.append("headline:(").append(QueryParserBase.escape(word)).append(")^8");
+            }
+        }
 
         // Boost description moderately
-        if (!topic.description.isEmpty()) sb.append("text:(").append(QueryParserBase.escape(topic.description)).append(")^6");
+        if (!topic.description.isEmpty()) {
+            String[] splitDesc = topic.description.split(" ");
+            for(String word : splitDesc) {
+                sb.append("text:(").append(QueryParserBase.escape(word)).append(")^6");
+                sb.append("summary:(").append(QueryParserBase.escape(word)).append(")^6");
+            }
+        }
 
         // Boost narrative lightly
         String posNarr = extractPositiveNarrative(topic.narrative);
-        if (!posNarr.isEmpty()) sb.append("text:(").append(QueryParserBase.escape(posNarr)).append(")^2.5");
-
+        if (!posNarr.isEmpty()) {
+            String[] splitNarr = posNarr.split(" ");
+            for(String word : splitNarr) {
+                sb.append("text:(").append(QueryParserBase.escape(word)).append(")^2.5");
+            }
+        }
 
         return sb.toString().trim();
     }
